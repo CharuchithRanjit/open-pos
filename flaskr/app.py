4735a7c883bd9efb6eee4a639e53f4c45637da02
Jadon <jadonzufall@gmail.com>
2021-06-27 21:26:46 -0400

    Created 2 new html templates updated app.py

--- flaskr/app.py
+++ flaskr/app.py
@@ -10,12 +10,16 @@ from flask import request
 from flask import url_for
 
 from flaskr.db import get_db
-from flaskr.auth import *
+from flaskr.auth import signin
+from flaskr.auth import signup
+from flaskr.auth import signout
+from flaskr.auth import ses_signin
 
 
 bp = Blueprint("app", __name__)
 
 
+# The main login page
 @bp.route('/')
 def index() -> object:
 	file_path = url_for('templates/main', filename='index.html')
@@ -23,6 +27,7 @@ def index() -> object:
 	return render()
 
 
+# The sign in page
 @bp.route('/signin')
 def signin() -> object:
 	file_path = url_for('templates/auth', filename='signin.html')
@@ -30,6 +35,7 @@ def signin() -> object:
 	return render()
 
 
+# The sign up page
 @bp.route('/signup')
 def signup() -> object:
 	file_path = url_for('templates/auth', filename='signup.html')
@@ -37,11 +43,27 @@ def signup() -> object:
 	return render()
 
 
+# The sign out page
 @bp.route('/signout')
 def signout() -> object:
 	file_path = url_for('templates/auth', filename='signout.html')
-	render = lamda : redner_template(file_path)
+	render = lambda : redner_template(file_path)
 	return render()
 
+
+# The home page for the user
+@bp.route('/user/home')
+def user_home() -> object:
+	file_path url_for('templates/user', filename='home.html')
+	render = lambda : render_template(file_path)
+	return render()
+
+
+# The admin panel
+@bp.route('/admin/home')
+def admin_home() -> object:
+	file_path = url_for('templates/admin', filename='home.html')
+	render = lambda : render_template(file_path)
+	return render()
 
 
