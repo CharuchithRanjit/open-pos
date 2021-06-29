ca1b489658ae41743237145f56a1176ef11bba14
Jadon <jadonzufall@gmail.com>
2021-06-28 22:01:38 -0400

    Typo

--- run/run.py
+++ run/run.py
@@ -1,7 +1,7 @@
 # This is where you start the webserver from
 
 from flask import Flask
-from flakr.__init__ import create_app
+from flaskr.__init__ import create_app
 
 if __name__ == "__main__":
 	app = create_app()
