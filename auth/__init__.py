80769fb6411676a1d1ab61ff1a71cfc9ff9057ad
Jadon <jadonzufall@gmail.com>
2021-07-03 04:35:33 -0400

    Thanks to ant honey lamb was able to clean up __init__ folders.  Also removed some useless things I added into them and I wasn't sure why.

--- auth/__init__.py
+++ auth/__init__.py
@@ -1,39 +1,14 @@
-"""
-Short Description
+# Python Dependencies
+import os
 
-Classes:
-User
-
-Functions:
+# Exsternal Dependencies
 None
 
-Misc variables:
-DATABASE_KEY (str) -- The key for the database
-DATABASE_PASSWORD (str) -- The password for the database
-DATABASE_URL (str) -- The url for the database
-"""
-
-__author__ = "Jadon Zufall"
-__copyright__ = "Copyright 2021, Jadon Zufall"
-__credits__ = ["Jadon Zufall"]
-__license__ = "MIT"
-__version__ = "dev_non_functional"
-__maintainer__ = "Jadon Zufall"
-__email__ = "jadonzufall@gmail.com"
-__status__ = "non_functional"
-
-import os
+# Local Dependencies
+None
 
 
 def return_path() -> str:
-	"""
-	Returns the root file path for dependencies.
-
-	Parameters:
-	None
-
-	Returns:
-	str : The root file path
-	"""
+	""" Returns the root file path """
 	path = os.path.dirname(os.path.dirname(__file__))
 	return path
--- local_client/__init__.py
+++ local_client/__init__.py
@@ -1,27 +1,14 @@
-__author__ = "Jadon Zufall"
-__copyright__ = "Copyright 2021, Jadon Zufall"
-__credits__ = ["Jadon Zufall"]
-__license__ = "MIT"
-__version__ = "dev_non_functional"
-__maintainer__ = "Jadon Zufall"
-__email__ = "jadonzufall@gmail.com"
-__status__ = "non_functional"
-
+# Python Dependencies
 import os
+
+# Exsternal Dependencies
+None
+
+# Local Dependencies
+None
 
 
 def return_path() -> str:
-	"""
-	Returns the correct path for local dependencies.
-
-	Gets the current file path, then loops up two files and finally adds open-pos file path to the end.
-
-	Parameters:
-	None
-
-	Returns:
-	str : The root file path
-	"""
-
+	""" Returns the root file path """
 	path = os.path.dirname(os.path.dirname(__file__))
 	return path
